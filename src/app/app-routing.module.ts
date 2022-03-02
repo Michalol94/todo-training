cf0f8c0cad4ace6e3208a95e46a196824bc5603d
Michalol94 <michal.olech94@gmail.com>
2022-03-02 19:10:43 +0100

    abcds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePagePageModule } from './pages/home-page.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'onas', 
+        loadChildren: () => HomePagePageModule
+      },
+  { 
+        path: 'abc', 
+        loadChildren: () => AbcPageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HomePagePageModule],
