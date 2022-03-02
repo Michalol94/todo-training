4570f5ef3e28385cb405ec94094e82aa6fe4d28e
Michalol94 <michal.olech94@gmail.com>
2022-03-02 18:41:59 +0100

    secundo commito

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePagePageModule } from './pages/home-page.page-module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomePagePageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
