3bd76fe9d4dc223233a0c190648d68cdc08fb65e
Michalol94 <michal.olech94@gmail.com>
2022-03-14 07:22:16 +0100

    B4 queryParams

--- projects/team/src/lib/adapters/primary/ui/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component.ts
@@ -6,4 +6,22 @@ export class OurTeamComponent {
 
     param$ = this.activatedRoute.params;
     constructor(private activatedRoute: ActivatedRoute) { }
+
+    images = {
+
+        image1: {
+            src: 'https://fwcdn.pl/fph/07/49/120749/185892.3.jpg',
+            alt: 'Zygzak Mcqueen'
+        },
+
+        image2: {
+            src: 'https://inforiders.pl/wp-content/uploads/2015/12/TheStig_00.jpg',
+            alt: 'Stig'
+        },
+
+        image3: {
+            src: 'https://www.youandi.com/sites/default/files/styles/lead_image_800x500/public/field/image/Daniel%20Craig_0.jpg',
+            alt: 'Bond'
+        }
+    };
 }
