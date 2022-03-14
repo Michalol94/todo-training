84ed5f743d7f0039ad3ef87e6afb702fab0428ba
Michalol94 <michal.olech94@gmail.com>
2022-03-14 22:52:56 +0100

    form alert

--- projects/messages/src/lib/adapters/primary/ui/contact-form.component-module.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component-module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactFormComponent } from './contact-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
-@NgModule({ imports: [CommonModule],
-  	declarations: [ContactFormComponent],
-  	providers: [],
-  	exports: [ContactFormComponent] })
+@NgModule({
+	imports: [CommonModule, ReactiveFormsModule],
+	declarations: [ContactFormComponent],
+	providers: [],
+	exports: [ContactFormComponent]
+})
 export class ContactFormComponentModule {
 }
--- projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
@@ -1,5 +1,14 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({ selector: 'lib-contact-form', templateUrl: './contact-form.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class ContactFormComponent {
+    readonly messageForm: FormGroup = new FormGroup({ email: new FormControl(), text: new FormControl() });
+
+    onFormSubmited(messageForm: FormGroup) {
+        alert(
+            `email: ${messageForm.value.email} oraz text: ${messageForm.value.text}`
+        );
+    }
+
 }
