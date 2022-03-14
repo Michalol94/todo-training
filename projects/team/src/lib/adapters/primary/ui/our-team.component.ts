ef0fdd92f77be6ab98b67bacfb1b7b6509acbd1b
Michalol94 <michal.olech94@gmail.com>
2022-03-14 10:44:54 +0100

    Dynamiczna strona z obrazkami

--- projects/team/src/lib/adapters/primary/ui/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component.ts
@@ -1,27 +1,23 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+
 
 @Component({ selector: 'lib-our-team', templateUrl: './our-team.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class OurTeamComponent {
-
-    param$ = this.activatedRoute.params;
-    constructor(private activatedRoute: ActivatedRoute) { }
-
     images = {
 
         image1: {
             src: 'https://fwcdn.pl/fph/07/49/120749/185892.3.jpg',
-            alt: 'Zygzak Mcqueen'
+            name: 'Zygzak Mcqueen'
         },
 
         image2: {
             src: 'https://inforiders.pl/wp-content/uploads/2015/12/TheStig_00.jpg',
-            alt: 'Stig'
+            name: 'Stig'
         },
 
         image3: {
             src: 'https://www.youandi.com/sites/default/files/styles/lead_image_800x500/public/field/image/Daniel%20Craig_0.jpg',
-            alt: 'Bond'
+            name: 'Bond'
         }
     };
 }
--- src/app/pages/employee-details.page.ts
+++ src/app/pages/employee-details.page.ts
@@ -1,8 +1,11 @@
+import { partitionArray } from '@angular/compiler/src/util';
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({ selector: 'lib-employee-details-page', templateUrl: './employee-details.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class EmployeeDetailsPage {
     param$ = this.activatedRoute.params;
+    queryParams$ = this.activatedRoute.queryParamMap;
+
     constructor(private activatedRoute: ActivatedRoute) { }
 }
