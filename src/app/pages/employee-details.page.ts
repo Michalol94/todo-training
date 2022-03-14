d79621c421b891a872ae160fe44220b1c11b9add
Michalol94 <michal.olech94@gmail.com>
2022-03-14 11:54:48 +0100

    employee-details-smart-component-allsaved

--- src/app/pages/employee-details.page.ts
+++ src/app/pages/employee-details.page.ts
@@ -1,11 +1,11 @@
 import { partitionArray } from '@angular/compiler/src/util';
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+// import { ActivatedRoute } from '@angular/router';
 
 @Component({ selector: 'lib-employee-details-page', templateUrl: './employee-details.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class EmployeeDetailsPage {
-    param$ = this.activatedRoute.params;
-    queryParams$ = this.activatedRoute.queryParamMap;
+    // param$ = this.activatedRoute.params;
+    // queryParams$ = this.activatedRoute.queryParamMap;
 
-    constructor(private activatedRoute: ActivatedRoute) { }
+    // constructor(private activatedRoute: ActivatedRoute) {}
 }
