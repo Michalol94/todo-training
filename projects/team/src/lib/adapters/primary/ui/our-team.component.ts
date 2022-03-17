359d1cabc7aae7e580415a0383588e997d009cf7
Michalol94 <michal.olech94@gmail.com>
2022-03-17 07:43:16 +0100

    subDTO changed to list

--- projects/team/src/lib/adapters/primary/ui/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component.ts
@@ -25,11 +25,16 @@ export class OurTeamComponent {
             imageUrl: 'https://fwcdn.pl/fph/07/49/120749/185892.3.jpg',
             name: 'Zygzak Mcqueen',
             bio: 'Zygzak McQueen jest najszybszą wyścigówką świata. Dawniej był lekko arogancki. Liczyła się dla niego głowne wygrane oraz dobre wrażenie.Twierdził że nie potrzebuje przyjaciół, bo ma sporo fanów na trybunach. Wszystko to się jednak zmieniło gdy pewnego dnia trafił do Chłodnicy Górskiej gdzie przypadkowo zniszczył drogę i musiał ją naprawić.Tam poznał przyjaciół i zmienił swój charakter.',
-            department:
-            {
-                name: 'Nascar masters',
-                employeeCount: 95
-            }
+            department: [
+                {
+                    name: 'Nascar masters',
+                    employeeCount: 95
+                },
+                {
+                    name: 'Nascar masters2',
+                    employeeCount: 95
+                }
+            ]
 
 
         },
@@ -39,12 +44,12 @@ export class OurTeamComponent {
             imageUrl: 'https://inforiders.pl/wp-content/uploads/2015/12/TheStig_00.jpg',
             name: 'Stig',
             bio: 'Tożsamość Stiga od wielu lat była tajemnicą. O wcielanie się w jego rolę wielokrotnie podejrzewani byli m.in. Damon Hill, Russ Swift, Mark Blundell i Julian Bailey. Jednak 19 stycznia 2009 roku brytyjska gazeta The Times podała, iż za białym kaskiem od zawsze krył się słynny kierowca wyścigowy Ben Collins.',
-            department:
-            {
-                name: 'Anonymous',
-                employeeCount: 1
-            }
-
+            department: [
+                {
+                    name: 'Anonymous',
+                    employeeCount: 1
+                }
+            ]
         },
 
         {
@@ -52,12 +57,12 @@ export class OurTeamComponent {
             imageUrl: 'https://www.youandi.com/sites/default/files/styles/lead_image_800x500/public/field/image/Daniel%20Craig_0.jpg',
             name: 'James Bond',
             bio: 'James Bond, także agent 007 – postać fikcyjna, szpieg brytyjskiej Secret Intelligence Service (MI6), bohater szeregu powieści Iana Fleminga, protagonista w wielu powieściach i opowiadaniach autorstwa Iana Flemina.',
-            department:
-            {
-                name: 'Secret Service',
-                employeeCount: 0o7
-            }
-
+            department: [
+                {
+                    name: 'Secret Service',
+                    employeeCount: 0o7
+                }
+            ]
         }
 
     ]);
